improved plots, fixed control input scaling, added friction reduction as a disturbance

leap_c/examples/vehicle_steering/env.py
@@ -24,14 +24,15 @@ from leap_c.examples.vehicle_steering.utils import (
 
 @dataclass(kw_only=True)
 class VehicleParams:
-    cf: float | ca.SX = 72705.0     # [N/rad]   front cornering stiffness (for one tire)
-    cr: float | ca.SX = 72705.0     # [N/rad]   rear cornering stiffness (for one tire)
-    m:  float | ca.SX = 1600.0      # [kg]      vehicle mass
-    vx: float | ca.SX = 60 / 3.6    # [m/s]     longitudinal vehicle speed
-    lf: float | ca.SX = 1.311       # [m]       distance from the center of gravity to the front axle
-    lr: float | ca.SX = 1.311       # [m]       distance from the center of gravity to the rear axle
-    iz: float | ca.SX = 2394.0      # [kg*m^2]  vehicle moment of inertia
-    isw:float | ca.SX = 13          # [-]       steering ratio
+    cf:     float | ca.SX = 72705.0                 # [N/rad]   front cornering stiffness (for one tire)
+    cr:     float | ca.SX = 72705.0                 # [N/rad]   rear cornering stiffness (for one tire)
+    m:      float | ca.SX = 1600.0                  # [kg]      vehicle mass
+    vx:     float | ca.SX = 60 / 3.6                # [m/s]     longitudinal vehicle speed
+    lf:     float | ca.SX = 1.311                   # [m]       distance from the center of gravity to the front axle
+    lr:     float | ca.SX = 1.311                   # [m]       distance from the center of gravity to the rear axle
+    iz:     float | ca.SX = 2394.0                  # [kg*m^2]  vehicle moment of inertia
+    isw:    float | ca.SX = 13                      # [-]       steering ratio
+    sw_max: float | ca.SX = float(np.deg2rad(90))   # [rad] maximum steering wheel angle (wheel angle = steering wheel angle / steering ratio)
 
 
 def get_A_cont(
@@ -137,7 +138,7 @@ class VehicleSteeringEnv(gym.Env):
         dt: float = 0.05,
         max_time: float = 10.0,  # TODO: check max_time value
         vehicle_params: VehicleParams = VehicleParams(),
-        steer_max: float = np.deg2rad(90),  # [rad]
+        steer_max: float | None = None,
         road_bank_angle: float = 0.0,  # [rad]
         reference_type: str = "straight",
         # TODO: check obs_space limits for error derivatives
@@ -153,11 +154,15 @@ class VehicleSteeringEnv(gym.Env):
     ):
         super().__init__()
 
+        self.vehicle_params = vehicle_params
         self.init_state_dist = init_state_dist
         self.observation_space = observation_space
+        if steer_max is None:
+            steer_max = vehicle_params.sw_max
+        # action is the wheel steering angle (delta)
         self.action_space = gym.spaces.Box(
-            low=np.array([-steer_max/vehicle_params.isw]),
-            high=np.array([steer_max/vehicle_params.isw]),
+            low=np.array([-steer_max / vehicle_params.isw]),
+            high=np.array([steer_max / vehicle_params.isw]),
             dtype=np.float64,
         )
         self.dt = dt
@@ -169,14 +174,14 @@ class VehicleSteeringEnv(gym.Env):
         self.reference_type = reference_type
         self.X = 0  # for tracking the global position of the vehicle
         self.vx = vehicle_params.vx
+        self.env_changed = False
 
         if render_mode is not None:
             raise NotImplementedError("Rendering is not implemented yet.")
         
 
     def step(self, action: np.ndarray) -> tuple[Any, float, bool, bool, dict]:
-        self.action_to_take = action
-        self.u = action  # TODO: disturb action?
+        self.u = action
 
         road_bank_effect = np.sin(self.road_bank_angle) * np.array([0, 9.81, 0, 0])
 
@@ -189,7 +194,11 @@ class VehicleSteeringEnv(gym.Env):
             raise ValueError("Invalid reference type")
         u_aug = np.vstack((self.u, curvature*self.vx)).ravel()  # set the reference yaw rate as an input
 
-        self.X = self.X + self.vx * self.dt
+        self.X = self.X + self.vx * self.dt  # acceptably wrong (assumes small steering angles)
+
+        if self.X > 50 and not self.env_changed:
+            self._change_env_conditions()
+
         t_span = (0, self.dt)
         system_dynamics = lambda t, x: self.A @ x + self.B @ u_aug + road_bank_effect
         sol = solve_ivp(system_dynamics, t_span, self.state)
@@ -230,6 +239,7 @@ class VehicleSteeringEnv(gym.Env):
         plt.close("all")
 
         return self.state, {}
+    
 
     def _calculate_reward(self):
         # quadratic cost on lateral and heading errors, and their derivatives
@@ -246,6 +256,7 @@ class VehicleSteeringEnv(gym.Env):
             reward += 50
         
         return reward
+    
 
     def _is_done(self):
         # episode is done if the states (errors and derivatives) are close to the origin
@@ -266,6 +277,17 @@ class VehicleSteeringEnv(gym.Env):
 
         return done
     
+
+    def _change_env_conditions(self):
+        # reduce the friction coefficients to simulate a slippery road
+        self.vehicle_params.cf = 0.2 * self.vehicle_params.cf
+        self.vehicle_params.cr = 0.2 * self.vehicle_params.cr
+        self.A, self.B = get_continuous_system(vehicle_params=self.vehicle_params)
+        print('Changed the environment conditions')
+        self.env_changed = True
+        return None
+    
+
     def render(self):
         print("rendering not implemented")
         return np.random.rand(40,40,3)  # return something so it doesn't break
@@ -295,8 +317,9 @@ if __name__ == '__main__':
     S[:, 0] = s    
     for i in range(n_iter - 1):
         lqr_input = ctrl(S[:, i])
-        assert lqr_input < env.action_space.high[0], "Control input exceeds action space"
-        assert lqr_input > env.action_space.low[0], "Control input exceeds action space"
+        if np.abs(lqr_input) > env.action_space.high[0]:
+            lqr_input = np.sign(lqr_input) * env.action_space.high[0]
+            Warning("Action is out of bounds")
 
         A[:, i] = lqr_input
         S[:, i+1], r, term, trunc, _ = env.step(A[:, i])

leap_c/examples/vehicle_steering/mpc.py
@@ -165,7 +165,8 @@ def export_parametric_ocp(
 
     ocp.constraints.x0 = x0
 
-    steer_max = np.deg2rad(90)
+    # control input is the wheel angle
+    steer_max = nominal_params["sw_max"] / nominal_params["isw"]
 
     # Box constraints on u
     ocp.constraints.lbu = np.array([-steer_max])

leap_c/examples/vehicle_steering/test_closed_loop.py
@@ -34,28 +34,31 @@ def run_closed_loop(
 
 def visualize_closed_loop(results: np.ndarray, env: VehicleSteeringEnv) -> None:
     import matplotlib.pyplot as plt
+    plt.rcParams['axes.xmargin'] = 0  # tight x range
+    vehicle_params = env.vehicle_params
+    isw = vehicle_params.isw
 
     e1 = results[:, 0]
     e2 = results[:, 2]
-    delta = np.rad2deg(results[:, 4])
+    delta_sw = isw * np.rad2deg(results[:, 4])
     x = results[:,5]
-    steer_max = np.rad2deg(env.action_space.high[0])
+    steer_max = isw * np.rad2deg(env.action_space.high[0])
 
     # results in the error frame
     fig, ax = plt.subplots(5, sharex=True, constrained_layout=True)
     fig.suptitle('Vehicle steering in closed loop (error frame)')
     ax[0].plot(x, e1)
     ax[0].set_ylabel('$e_y$ [m]')
-    ax[1].plot(x, e2)
-    ax[1].set_ylabel('$e_\psi$ [rad]')
+    ax[1].plot(x, np.rad2deg(e2))
+    ax[1].set_ylabel('$e_\psi$ [deg]')
     ax[2].plot(x, results[:, 1])
     ax[2].set_ylabel('$\dot{e}_y$ [m/s]')
-    ax[3].plot(x, results[:, 3])
-    ax[3].set_ylabel('$\dot{e}_\psi$ [rad/s]')
-    ax[4].plot(x, delta)
+    ax[3].plot(x, np.rad2deg(results[:, 3]))
+    ax[3].set_ylabel('$\dot{e}_\psi$ [deg/s]')
+    ax[4].plot(x, delta_sw)
     ax[4].axhline( steer_max, color="r", linestyle='--')
     ax[4].axhline(-steer_max, color="r", linestyle='--')
-    ax[4].set_ylabel('$\delta$ [deg]')
+    ax[4].set_ylabel('$\delta_\mathrm{sw}$ [deg]')
     ax[4].set_xlabel('$x$ [m]')
 
     # results in the inertial frame
@@ -70,14 +73,14 @@ def visualize_closed_loop(results: np.ndarray, env: VehicleSteeringEnv) -> None:
     ax[0].plot(x, y_ref, 'r--')    
     ax[0].set_ylabel('$y$ [m]')
 
-    ax[1].plot(x, psi)
-    ax[1].plot(x, psi_ref, 'r--')    
-    ax[1].set_ylabel('$\psi$ [rad]')
+    ax[1].plot(x, np.rad2deg(psi))
+    ax[1].plot(x, np.rad2deg(psi_ref), 'r--')
+    ax[1].set_ylabel('$\psi$ [deg]')
 
-    ax[2].plot(x, delta)
+    ax[2].plot(x, delta_sw)
     ax[2].axhline( steer_max, color="r", linestyle='--')
     ax[2].axhline(-steer_max, color="r", linestyle='--')
-    ax[2].set_ylabel('$\delta$ [deg]')
+    ax[2].set_ylabel('$\delta_\mathrm{sw}$ [deg]')
     ax[2].set_xlabel('$x$ [m]')
 
     plt.show(block=False)
@@ -90,8 +93,14 @@ def test_run_closed_loop(
     mpc: VehicleSteeringMPC, env: VehicleSteeringEnv, n_iter: int = 50
 ) -> None:
     sim_data = run_closed_loop(mpc=mpc, env=env, n_iter=n_iter)
+
+    delta = np.rad2deg(sim_data[:, 4])
+    steer_max = np.rad2deg(env.action_space.high[0])
+    assert np.all(np.abs(delta) < steer_max), "steering angle out of bounds"
+
     errors_small_enough = np.max(np.abs(sim_data[-1,[0,2]])) < MAX_FINAL_DIST
     error_derivatives_small_enough = np.max(np.abs(sim_data[-1,[1,3]])) < MAX_FINAL_VEL
+
     if not errors_small_enough or not error_derivatives_small_enough:
         print("system not stabilized")
     else: