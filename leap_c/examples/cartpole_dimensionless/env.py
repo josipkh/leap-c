fixed observation scaling, matching parameters in env and mpc

leap_c/examples/cartpole_dimensionless/env.py
@@ -60,7 +60,7 @@ class PendulumOnCartSwingupEnv(gym.Env):
         self.gravity = 9.81
         self.masscart = 1.0
         self.masspole = 0.1
-        self.length = 0.5  # TODO: check if this should be 0.8
+        self.length = 0.8
         self.Fmax = 80.0
         self.dt = 0.05
         self.max_time = 10.0
@@ -142,7 +142,7 @@ class PendulumOnCartSwingupEnv(gym.Env):
         if self.reset_needed:
             raise Exception("Call reset before using the step method.")
         
-        # scale the action if dimensionless
+        # scale the MPC output if dimensionless
         if dimensionless:
             action = action * self.masscart * self.gravity
 
@@ -344,7 +344,5 @@ class PendulumOnCartSwingupEnv(gym.Env):
 
 class PendulumOnCartBalanceEnv(PendulumOnCartSwingupEnv):
     def init_state(self, options: Optional[dict] = None) -> np.ndarray:
-        low, high = gym_utils.maybe_parse_reset_bounds(
-            options, -0.05, 0.05  # default low
-        )  # default high
+        low, high = gym_utils.maybe_parse_reset_bounds(options, -0.05, 0.05)
         return self.np_random.uniform(low=low, high=high, size=(4,))

leap_c/examples/cartpole_dimensionless/mpc.py
@@ -103,7 +103,7 @@ class PendulumOnCartMpcDimensionless(Mpc):
             exact_hess_dyn: If False, the contributions of the dynamics will be left out of the Hessian.
             cost_type: The type of cost to use, either "EXTERNAL" or "NONLINEAR_LS".
         """
-        params = params if params is not None else PARAMS  # type:ignore
+        params = params if params is not None else PARAMS.copy()  # type:ignore
 
         # non-dimensionalize the time
         if dimensionless:
@@ -138,17 +138,15 @@ class PendulumOnCartMpcDimensionless(Mpc):
             params["L55"] = params["L55"] * (F_scale)**2
 
         ocp = export_parametric_ocp(
-            nominal_param=params.copy(),
+            nominal_param=params,
             cost_type=cost_type,
             exact_hess_dyn=exact_hess_dyn,
-            name="pendulum_on_cart",
+            name="cartpole_dimensionless",
             learnable_param=learnable_params,
             N_horizon=N_horizon,
             tf=T_horizon_hat,
             Fmax=Fmax_hat,
         )
-
-        self.given_default_param_dict: dict[str, np.ndarray] = params  # type:ignore
 
         super().__init__(
             ocp=ocp,
@@ -211,12 +209,15 @@ def f_expl_expr(model: AcadosModel) -> ca.SX:
         a_scale = [F_scale]
         ds_scale = [x_scale/t_scale, theta_scale/t_scale, x_scale/t_scale**2, theta_scale/t_scale**2]
 
+        # RHS states
         for k in range(len(s_scale)):
             f_expl = ca.substitute(f_expl, s[k], s_scale[k]*s_hat[k])
 
+        # RHS actions
         for k in range(len(a_scale)):
             f_expl = ca.substitute(f_expl, a[k], a_scale[k]*a_hat[k])
 
+        # LHS (derivatives)
         for k in range(len(ds_scale)):
             f_expl[k] /= ds_scale[k]
 
@@ -375,7 +376,7 @@ def export_parametric_ocp(
     ocp.constraints.idxbx_0 = np.array([0, 1, 2, 3])
     ocp.constraints.x0 = np.array([0.0, np.pi, 0.0, 0.0])
 
-    ocp.constraints.lbu = np.array([-Fmax])
+    ocp.constraints.lbu = np.array([-Fmax])  # already dimensionless
     ocp.constraints.ubu = np.array([+Fmax])
     ocp.constraints.idxbu = np.array([0])
 

leap_c/examples/cartpole_dimensionless/task.py
@@ -56,7 +56,7 @@ class PendulumOnCartSwingupDimensionless(Task):
         learnable_params = ["xref2"]
 
         mpc = PendulumOnCartMpcDimensionless(
-            N_horizon=6,
+            N_horizon=5,
             T_horizon=0.25,
             learnable_params=learnable_params,
             params=params,  # type: ignore
@@ -82,12 +82,12 @@ class PendulumOnCartSwingupDimensionless(Task):
 
         mpc_param = MpcParameter(p_global=param_nn)  # type: ignore
 
-        # make the observation (x,dx,theta,dtheta) dimensionless
+        # make the observation (x,theta,dx,dtheta) dimensionless
         if dimensionless:
             l = PARAMS_SWINGUP["l"]
             g = PARAMS_SWINGUP["g"]
             obs[0][0] /= l
-            obs[0][1] /= l * np.sqrt(l/g)
+            obs[0][2] /= l * np.sqrt(l/g)
             obs[0][3] *= np.sqrt(l/g)
 
         return MpcInput(x0=obs, parameters=mpc_param)