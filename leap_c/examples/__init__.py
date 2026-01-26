90da80b0be01f4eff80b0c071892fa7a50e416ee
Josip Kir Hromatko <josipkh@gmail.com>
2026-01-26 16:45:34 +0100

    remove old dimensionless cart pole example

--- leap_c/examples/__init__.py
+++ leap_c/examples/__init__.py
@@ -3,4 +3,3 @@ import leap_c.examples.pointmass.task
 import leap_c.examples.cartpole.task
 import leap_c.examples.chain.task
 import leap_c.examples.mujoco.reacher.task
-import leap_c.examples.cartpole_dimensionless.task
